a713ff94f9a5282853778a8ef897538e751bf84a
zion <zian.qin@mail.utoronto.ca>
2016-08-22 14:55:14 -0400

    some ODB stuff

--- records/get_id.py
+++ records/get_id.py
@@ -1,6 +1,7 @@
-records = open('OBD_species_p1.txt','w')
+records = open('ODB_species_p1.txt','r')
 
 lines = records.readlines()
+# print(lines)
 
 unique_bacteria =  set()
 for line in lines:
@@ -9,9 +10,29 @@ for line in lines:
   if "#" in items[0]: # skip comments
     continue
   
-  unique_bacteria.add(items[1])
+  unique_bacteria.add("_".join(items[1:4]))
 
-for i in unique_bacteria:
-  print(i)
-  
 records.close()
+
+id_list = open('2.species_list','r')
+
+lines = id_list.readlines()
+
+dne = []
+
+for bacteria in unique_bacteria:
+  found = False
+  for l in lines:
+    line = l.split()
+    #print(line)
+    if (line[1] in bacteria) or (bacteria in line[1]):
+      print('{})\t{}'.format(line[0],line[1]))
+      found = True
+  
+  if not found:
+    dne.append(bacteria)
+  
+id_list.close()
+
+for i in dne:
+  print(i)
